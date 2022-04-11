cb9191ab1527155a7fcebd57c6b1420644bbc812
Kirill Fahredinov <92108744+woniderhaf@users.noreply.github.com>
2022-04-11 18:59:35 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -233,3 +233,7 @@ burger.addEventListener('click', () => {
         menuContent.style.display = 'none'
     } 
 })
+
+$('.mask-phone').mask('+7 (999) 999-99-99');
+$('.mask-time').mask('99:99');
+$('.mask-date').mask('99.99.9999');
